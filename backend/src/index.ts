7eae4987267210840d54c2a7104d74e3e855e2ce
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-12-30 14:46:54 +0000

    Slight DEV program

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import Fastify from "fastify";
+import "dotenv/config";
 const app = Fastify({
     logger: true
 })
+
 app.get('/', function (_request, reply) {
     reply.send({ hello: 'world' })
   })
   
   // Run the server!
-  app.listen({ port: 3001 }, function (err, _address) {
+  app.listen({ port: Number(process.env.PORT), host: String(process.env.HOST)}, function (err, _address) {
     if (err) {
       app.log.error(err)
       process.exit(1)
