f0e724c985425cef99198b2d3d7ee24f6037db0e
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-02-16 23:19:21 +0000

    Fix getListOfBots endpoint and delete others

--- pages/api/bots/[userEmail]/getListOfBots.ts
+++ pages/api/bots/[userEmail]/getListOfBots.ts
@@ -3,6 +3,16 @@ import db from "@/lib/db"
 export default async function handler(req: any, res: any) {
     // Parse userEmail
     const userEmail = req.query.userEmail.replace(".","-");
-    const botNames = await db.get(`_${userEmail}.botNames`)
+    //const botNames = await db.get(`_${userEmail}.botNames`)
+    const botNames = await db.bot.findMany({
+        where: {
+            user: {
+                email: userEmail
+            }
+        },
+        select: {
+            name: true
+        }
+    })
     res.status(200).json({data: botNames})
 }
